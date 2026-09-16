30a449cc8d3240898f87ec17a001cfce759e6dfd
toxhthcs <minhdinhcamau@gmail.com>
2026-09-16 08:27:04 +0700

    Sửa đường dẫn import supabaseClient

--- app/admin/qa-post/[id]/page.jsx
+++ app/admin/qa-post/[id]/page.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState, useCallback } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { supabase } from '../../../lib/supabaseClient'
+import { supabase } from '../../../../lib/supabaseClient'
 import styles from './qaPostDetail.module.css'
 
 function formatDateTime(iso) {
